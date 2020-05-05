2ae347612c87faea0cb465e1edc9c5c030c3af5f
jiyinyiyong <jiyinyiyong@gmail.com>
2020-05-06 00:59:00 +0800

    fix network state permission

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Get my IP address'),
+      home: MyHomePage(title: 'My IP address'),
     );
   }
 }
@@ -57,11 +48,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _setIp() async {
     String myAddress = await GetIp.ipAddress;
-    var formatter = new DateFormat('HH:mm:ss.sss');
+    var formatter = new DateFormat('HH:mm:ss');
     String now = formatter.format(new DateTime.now());
     setState(() {
       _myIp = myAddress;
-      _time = now;
+      _time =
+          "$now - ${(new DateTime.now().millisecond).toString().padLeft(3, '0')}";
     });
   }
 
@@ -71,44 +63,23 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               '$_myIp',
-              style: Theme.of(context).textTheme.display4,
+              style: TextStyle(
+                  fontSize: 100,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w100),
             ),
             Container(
-              height: 80,
+              height: 60,
             ),
             Text(
               '$_time',
